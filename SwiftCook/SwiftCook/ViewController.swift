48f881582f735b131cc59c19b5ea60a06467c9ff
dekich <794481112@qq.com>
2016-09-06 21:20:38 +0800

    Create Project
    创建项目文件

---
+++ SwiftCook/SwiftCook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCook
+//
+//  Created by Deki on 16/9/6.
+//  Copyright © 2016年 Deki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
